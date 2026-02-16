5ad7f0478e504aff022eb22525f632096e1fc144
Anuphap2 <anuphap.240348@gmail.com>
2026-02-16 11:41:53 +0700

    refactor

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,4 +1,3 @@
-
 import { Controller, Post, Body } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
@@ -6,20 +5,20 @@ import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
 @ApiTags('Payments')
 @Controller('payments')
 export class PaymentsController {
-    constructor(private readonly paymentsService: PaymentsService) { }
+  constructor(private readonly paymentsService: PaymentsService) {}
 
-    @ApiOperation({ summary: 'Create payment intent' })
-    @ApiResponse({ status: 201, description: 'Payment intent created.' })
-    @ApiBody({
-        schema: {
-            type: 'object',
-            properties: {
-                amount: { type: 'number', example: 1000 },
-            },
-        },
-    })
-    @Post('create-intent')
-    create(@Body() body: { amount: number }) {
-        return this.paymentsService.createPaymentIntent(body.amount);
-    }
+  @ApiOperation({ summary: 'Create payment intent' })
+  @ApiResponse({ status: 201, description: 'Payment intent created.' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        amount: { type: 'number', example: 1000 },
+      },
+    },
+  })
+  @Post('create-intent')
+  create(@Body() body: { amount: number }) {
+    return this.paymentsService.createPaymentIntent(body.amount);
+  }
 }
--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,10 +1,9 @@
-
 import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 
 @Module({
-    controllers: [PaymentsController],
-    providers: [PaymentsService],
+  controllers: [PaymentsController],
+  providers: [PaymentsService],
 })
-export class PaymentsModule { }
+export class PaymentsModule {}
